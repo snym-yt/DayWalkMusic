6a3527c514d35d363dd1693c96400ac5cb0647d5
Sonoyama Yuto <rokokoromayuge@icloud.com>
2023-08-15 16:02:49 +0900

    MacOSアップデート，プラグインをAssetsに格納，トライ用シーン作成

---
+++ Assets/Scripts/Try/SampleScene.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SampleScene : MonoBehaviour
+{
+    public void OnClick(){
+        Debug.Log("ここでプラグインを使う");
+    }
+}
